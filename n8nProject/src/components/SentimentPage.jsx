64eac00d03a5b04c0213180514b021c6c771be9e
PAPASANI NARENDRA SAI <137531548+NarendraPapasani@users.noreply.github.com>
2025-12-18 11:47:00 +0530

    Update webhook URL in SentimentPage component

--- n8nProject/src/components/SentimentPage.jsx
+++ n8nProject/src/components/SentimentPage.jsx
@@ -35,7 +35,7 @@ const SentimentPage = () => {
 
     try {
       const response = await fetch(
-        "https://narendra-sai-9092.app.n8n.cloud/webhook-test/d7d09f23-1436-4792-9b8b-a6ea389ffb7e",
+        "https://narendra-sai-9092.app.n8n.cloud/webhook/d7d09f23-1436-4792-9b8b-a6ea389ffb7e",
         {
           method: "POST",
           headers: {
